2799a520044712e02522ab524e9b0f8731dd7943
Baimuratov <ilvir.baymuratov@mail.ru>
2023-09-14 09:41:11 +0500

    START (add new project) begin new project |  pizza Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import AddPizzaForm from "./components/AddPizzaForm";
+import { Pizza } from "./models/Pizza";
+import { DisplayPizzas } from "./components/DisplayPizzas";
 
+const App: React.FC = () => {
+	const [pizzasList, setPizzasList] = React.useState<Pizza[]>([]);
 
-function App() {
-  return (
-    <div className="App">
-      New App!
-    </div>
-  );
-}
+	const addPizza = (newPizza: Pizza) => {
+		setPizzasList([...pizzasList, newPizza]);
+	};
+
+	return (
+		<div className="App">
+			<div className="wrap">
+				<span className="heading">Наша пиццерия</span>
+				<AddPizzaForm addPizza={addPizza} />
+				<DisplayPizzas pizzasList={pizzasList} />
+			</div>
+		</div>
+	);
+};
 
 export default App;
