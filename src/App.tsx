49561d22570f4db243780e37849a2b7601694ba2
Baimuratov <ilvir.baymuratov@mail.ru>
2023-09-16 22:19:28 +0500

    add pizza addPizzaForm add new pizza and delete pizza and add Local Storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import AddPizzaForm from "./components/AddPizzaForm";
 import { Pizza } from "./models/Pizza";
@@ -8,15 +8,41 @@ const App: React.FC = () => {
 	const [pizzasList, setPizzasList] = React.useState<Pizza[]>([]);
 
 	const addPizza = (newPizza: Pizza) => {
-		setPizzasList([...pizzasList, newPizza]);
+		const newPizzasList = [...pizzasList, newPizza]
+		setPizzasList(newPizzasList);
+		localStorage.setItem("pizzasState", JSON.stringify(newPizzasList))
 	};
+
+	const updatePizza = (newPizza: Pizza) => {
+		const newPizzasList = pizzasList.map((pizza) => (pizza.id === newPizza.id ? newPizza : pizza))
+		setPizzasList(newPizzasList);
+		localStorage.setItem("pizzasState", JSON.stringify(newPizzasList))
+	};
+
+	const deletePizza = (id: number) => {
+		const newPizzasList = pizzasList.filter((pizza) => pizza.id !== id)
+		setPizzasList(newPizzasList)
+		localStorage.setItem("pizzasState", JSON.stringify(newPizzasList))
+	};
+
+	useEffect(() => {
+		const pizzasState = localStorage.getItem("pizzasState")
+		if(pizzasState) {
+			let res = JSON.parse(pizzasState)
+			setPizzasList(res)
+		}
+	}, [])
 
 	return (
 		<div className="App">
 			<div className="wrap">
 				<span className="heading">Наша пиццерия</span>
 				<AddPizzaForm addPizza={addPizza} />
-				<DisplayPizzas pizzasList={pizzasList} />
+				<DisplayPizzas
+					pizzasList={pizzasList}
+					updatePizza={updatePizza}
+					deletePizza={deletePizza}
+				/>
 			</div>
 		</div>
 	);
